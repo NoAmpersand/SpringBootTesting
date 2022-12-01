ff38ca8000a9b99948cf3b9300cfedb1cf772164
Jad L <92789238+NoAmpersand@users.noreply.github.com>
2022-12-01 04:17:11 +0100

    Setting up employeeService
    Juggling dependency injections and annotations

--- src/main/java/com/example/demo/employee/EmployeeService.java
+++ src/main/java/com/example/demo/employee/EmployeeService.java
@@ -1,9 +1,12 @@
 package com.example.demo.employee;
 
+import org.springframework.stereotype.Service;
+
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
 
+@Service //same as Component, just a semantics thing for readability
 public class EmployeeService {
     public List<Employee> getEmployees(){
         return List.of(
